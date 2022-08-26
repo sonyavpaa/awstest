1d16a1c33dcbd076fbad95a8cf5a2f05aefe04ff
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-08-26 12:59:03 +0300

    Sonja committed Eric, again

--- src/components/Coles/coles.js
+++ src/components/Coles/coles.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import backgroundVideo from "./backgroundVideo.mp4";
+import backgroundVideo from "./backgroundVideo.mp4";
 import "./coles.css";
 
 const Coles = () => {
@@ -8,7 +8,7 @@ const Coles = () => {
       <h1>Eric Coles Coded Here</h1>
       <h2>Relax and have a cocktail on me</h2>
       <div className="video">
-        {/* <video autoPlay muted loop src={backgroundVideo} type="video/mp4" /> */}
+        <video autoPlay muted loop src={backgroundVideo} type="video/mp4" />
       </div>
     </div>
   );
