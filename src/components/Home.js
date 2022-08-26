b81d7cdc8ec575da99721a6fafb01e46220ed46b
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-08-26 12:52:33 +0300

    Sonja committted again

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Jeremy from "./Jeremy"; 
+import Jeremy from "./Jeremy";
+import Sonja from "./Sonja";
 
 const Home = () => {
   return (
     <>
       <div>Home is here</div>
-      <Jeremy/>
+      <Jeremy />
+      <Sonja />
     </>
   );
 };
