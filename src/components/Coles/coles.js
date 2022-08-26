b4e29e6225125a304e1e6316e445d05f1960e600
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-08-26 13:50:18 +0300

    small changes

--- src/components/Coles/coles.js
+++ src/components/Coles/coles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import backgroundVideo from "./backgroundVideo.mp4";
+import BackgroundVideo from "./BackgroundVideo.mp4";
 import "./coles.css";
 
 const Coles = () => {
@@ -8,7 +8,7 @@ const Coles = () => {
       <h2>Eric Coles Coded Here</h2>
       <h2>Relax and have a cocktail on us</h2>
       <div className="video">
-        <video autoPlay muted loop src={backgroundVideo} type="video/mp4" />
+        <video autoPlay muted loop src={BackgroundVideo} type="video/mp4" />
       </div>
     </div>
   );
