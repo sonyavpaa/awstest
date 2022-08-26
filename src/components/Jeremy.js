4e0054b54264abf6b208d12bf1cc4c65f7ffbcae
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-08-26 12:50:42 +0300

    Sonja added CICD badge

--- src/components/Jeremy.js
+++ src/components/Jeremy.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Jeremy = () => {
-    return (
-        <>
-            <div>MrJay says <b>"Pikachuuuuuuuuu. Mina valitsen sinua! Tee Thunder Bolt! "</b></div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        MrJay says{" "}
+        <b>"Pikachuuuuuuuuu. Mina valitsen sinua! Tee Thunder Bolt! "</b>
+      </div>
+    </>
+  );
 };
 
 export default Jeremy;
