ff4a1bcee113aed391cf84a6101dfe51b1e7f482
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-08-26 12:51:29 +0300

    github is different each time, Coles Commit

---
+++ src/components/Coles/coles.js
@@ -0,0 +1,17 @@
+import React from "react";
+import backgroundVideo from "./backgroundVideo.mp4";
+import "./coles.css";
+
+const Coles = () => {
+  return (
+    <div>
+      <h1>Eric Coles Coded Here</h1>
+      <h2>Relax and have a cocktail on me</h2>
+      <div className="video">
+        <video autoPlay muted loop src={backgroundVideo} type="video/mp4" />
+      </div>
+    </div>
+  );
+};
+
+export default Coles;
